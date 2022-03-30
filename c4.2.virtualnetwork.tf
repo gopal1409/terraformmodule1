fdcbe43ba5c2507656b9e5fd41220db1e4cd39c1
gopal das <gopal1409@gmail.com>
2022-03-30 19:19:56 +0530

    terraform with module

--- c4.2.virtualnetwork.tf
+++ c4.2.virtualnetwork.tf
@@ -30,6 +30,9 @@ module "vnet" {
     costcenter = "it"
   }
   # insert the 2 required variables here
+  /*depends_on = [
+    azurerm_resource_group.myrg
+  ]*/
 }
 
 resource "azurerm_public_ip" "mypublicip" {
